39753a576849c15d5c0da7c74439203ecd7eb4d3
marvinxkiyingi <marvin96k@gmail.com>
2021-10-04 20:33:33 +0200

    added a lottie animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Lottie from 'react-lottie';
+import constructionImg from './Animations/construction.json';
 
 function App() {
+  const constructionImgData = {
+    loop: true,
+    autoplay: true,
+    animationData: constructionImg,
+    rendererSettings: {
+      preserveAspectRatio: 'xMidYMid slice',
+    },
+  };
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>PortfolioWebsite 2.0</p>
+        <div className="constructionAnimation">
+          <Lottie
+            options={constructionImgData}
+            // height={400} width={400}
+          />
+        </div>
       </header>
     </div>
   );
