edc422d5b1596ed201162e2f060e741215ee0422
marvinxkiyingi <marvin96k@gmail.com>
2022-04-10 23:53:11 +0200

    styled(Projects & Project): made simple styling for mobile and desktop.

--- src/components/Projects/Project.tsx
+++ src/components/Projects/Project.tsx
@@ -1,23 +1,118 @@
+import { Box, Button, Typography, styled, Stack } from '@mui/material';
 import { IProject } from '../../models/IProject';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import OpenInNewIcon from '@mui/icons-material/OpenInNew';
 
+const ProjectContainer = styled(Box)(({ theme }) => ({
+  width: '90%',
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+
+  '.title': {
+    ...theme.typography.h6,
+    lineHeight: 'unset',
+    marginTop: theme.spacing(2),
+  },
+  '.title,.description,.ctaButtonContainer': {
+    width: '90%',
+    [theme.breakpoints.up('sm')]: {
+      width: '63%',
+    },
+  },
+  '.projectImgContainer,.description,.ctaButtonContainer': {
+    marginBottom: theme.spacing(2),
+  },
+
+  '.projectImgContainer': {
+    width: '90%',
+    position: 'relative',
+    display: 'flex',
+    justifyContent: 'center',
+    '.desktopImg': {
+      width: '100%',
+      borderRadius: 5,
+      padding: '0.5rem',
+      backgroundColor: theme.palette.primary.main,
+      [theme.breakpoints.up('sm')]: {
+        width: '70%',
+      },
+    },
+    '.mobileImg': {
+      position: 'absolute',
+      width: '20%',
+      top: '31%',
+      left: '82%',
+      [theme.breakpoints.up('sm')]: {
+        width: '17%',
+        top: '12%',
+        left: '74%',
+      },
+    },
+  },
+  '.ctaButtonContainer': {
+    [theme.breakpoints.up('md')]: {
+      marginBottom: 'unset',
+    },
+    '.cta': {
+      minWidth: 130,
+    },
+    '&>*+*': {
+      // marginLeft: theme.spacing(1),
+      [theme.breakpoints.up('xs')]: {
+        marginLeft: theme.spacing(2),
+      },
+    },
+  },
+}));
 export const Project = (project: IProject) => {
-  console.log(project);
   return (
-    <div>
-      <h1>{project.title}</h1>
-      <div>
-        <img src={project.desktopImg} alt="project img" />
-        <img src={project.mobileImg} alt="project img" />
-      </div>
-      <p>{project.description}</p>
-      <div>
-        <button>
-          <a href={project.githubLink}>Github</a>
-        </button>
-        <button>
-          <a href={project.livePage}>LivePage</a>
-        </button>
-      </div>
-    </div>
+    <ProjectContainer className="projectContainer">
+      {project && (
+        <>
+          <Typography className="title" component={'h1'}>
+            {project.title}
+          </Typography>
+          <Box className="projectImgContainer">
+            <img
+              className="desktopImg"
+              src={project.desktopImg}
+              alt="project img desktop"
+            />
+            <img
+              className="mobileImg"
+              src={project.mobileImg}
+              alt="project img mobile"
+            />
+          </Box>
+          <Typography className="description" component={'p'}>
+            {project.description}
+          </Typography>
+          <Stack
+            className="ctaButtonContainer"
+            direction="row"
+            justifyContent="center"
+            alignItems="center"
+          >
+            <Button
+              className="cta"
+              variant="contained"
+              href={project.githubLink}
+              endIcon={<GitHubIcon />}
+            >
+              Github
+            </Button>
+            <Button
+              className="cta"
+              variant="outlined"
+              href={project.livePage}
+              endIcon={<OpenInNewIcon />}
+            >
+              Livepage
+            </Button>
+          </Stack>
+        </>
+      )}
+    </ProjectContainer>
   );
 };
--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -1,3 +1,4 @@
+import { Typography } from '@mui/material';
 import { Box, styled } from '@mui/system';
 import { IProject } from '../../models/IProject';
 import { Project } from './Project';
@@ -7,22 +8,45 @@ const ProjectsContainer = styled(Box)(({ theme }) => ({
   minHeight: '100vh',
   maxWidth: 2520,
   margin: '0 auto',
+
+  '.projectsContainerHeading': {
+    textAlign: 'center',
+    lineHeight: 'unset',
+    // @ts-ignore
+    ...theme.typography.h4,
+    padding: theme.spacing(1, 1, 2),
+  },
+  '.projectsContent': {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    [theme.breakpoints.up('md')]: {
+      display: 'grid',
+      gridTemplateColumns: 'repeat(2, minmax(0, 1fr))',
+      gridGap: 20,
+    },
+  },
 }));
+
 export const Projects = () => {
   return (
-    <ProjectsContainer id="Portfolio">
+    <ProjectsContainer className="projectsContainer" id="Portfolio">
+      <Typography className="projectsContainerHeading" component="h1">
+        Portfolio
+      </Typography>
       <Box className="projectsContent">
-        {ProjectList.map((project: IProject, index: number) => (
-          <Project
-            key={index}
-            title={project.title}
-            desktopImg={project.desktopImg}
-            mobileImg={project.mobileImg}
-            description={project.description}
-            githubLink={project.githubLink}
-            livePage={project.livePage}
-          />
-        ))}
+        {ProjectList &&
+          ProjectList.map((project: IProject, index: number) => (
+            <Project
+              key={index}
+              title={project.title}
+              desktopImg={project.desktopImg}
+              mobileImg={project.mobileImg}
+              description={project.description}
+              githubLink={project.githubLink}
+              livePage={project.livePage}
+            />
+          ))}
       </Box>
     </ProjectsContainer>
   );
